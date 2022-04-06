3408e3937d56979b1b4f59ccb744dab34d12b34a
Amit Tiwari <amit.tiwari96@gmail.com>
2022-04-06 15:01:11 -0400

    Added first test.

--- app/src/test/java/at/hkr/interview/CountingValleyTest.java
+++ app/src/test/java/at/hkr/interview/CountingValleyTest.java
@@ -1,11 +1,22 @@
 package at.hkr.interview;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CountingValleyTest {
 
+    CountingValley countingValley;
+
+    @Before
+    void setUp(){
+        countingValley = new CountingValley();
+    }
+
     @Test
     public void testCountingValleys() {
-        
+        int valleyCount = countingValley.countingValleys(8, "DDUUUUDD");
+        assertEquals(1, valleyCount);
     }
 }
